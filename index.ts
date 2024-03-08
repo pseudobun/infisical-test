1ed4e42adab503e6b3547169319614ba8247d60c
Urban Vidovič <urbanfoundit@gmail.com>
2024-03-09 00:09:34 +0100

    feat: infisical integration
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { InfisicalClient } from "@infisical/sdk";
+
+const client = new InfisicalClient({
+  clientId: process.env.INFISICAL_CLIENT_ID,
+  clientSecret: process.env.INFISICAL_SECRET,
+});
+
+const name = await client.getSecret({
+  environment: "dev",
+  projectId: "b8b3748e-90d2-49e5-b88e-7da806b0c8c5",
+  path: "/test",
+  type: "shared",
+  secretName: "TEST_VALUE",
+});
+
+console.log(name.secretValue);
